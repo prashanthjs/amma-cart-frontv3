3aa6e1e65c86c67a7ca7d9ddca199dfefc0c5ffe
Prashanth Pratapagiri <prashanthp@cpwonlinesolutions.com>
2016-03-16 00:58:18 +0000

    added some files

---
+++ bk/user_bk/controllers/user.gallery.controller.ts
@@ -0,0 +1,26 @@
+export class UserGalleryController {
+
+
+    public images = [];
+    public $mdDialog;
+    public currentImage;
+    /* @ngInject */
+    constructor($mdDialog, images, image) {
+        this.currentImage = image;
+        this.images = images;
+        this.$mdDialog = $mdDialog;
+    }
+
+    next() {
+        var index = this.images.indexOf(this.currentImage);
+        index = index + 1 < this.images.length ? index + 1 : 0;
+        this.currentImage = this.images[index];
+    }
+
+    prev() {
+        var index = this.images.indexOf(this.currentImage);
+        index = index - 1 < 0 ? this.images.length - 1 : index - 1;
+        this.currentImage = this.images[index];
+    }
+
+}
